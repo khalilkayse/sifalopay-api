74544956a8682718cd456fa9155ba9bb459b210d
Khalil <khalil@Khalils-Mac-mini.local>
2024-03-26 14:53:46 +0300

    updated zaad gateway

--- core.php
+++ core.php
@@ -431,11 +431,11 @@ function verify_token($user, $token){
 
 function get_merchant_API($gateway, $merchant_id = 1){
 
-    $merchant_id = 33;
+    // $merchant_id = 33;
 
-    // if(!check_merchantAPI($gateway, $merchant_id)){
-    //    $merchant_id = 1;
-    // }
+    if(!check_merchantAPI($gateway, $merchant_id)){
+       $merchant_id = 1;
+    }
     // check if merchant has api 
   $api_keys = [];
     $data = getData("SELECT gateway, username, password, merchant_no, device_type, machine_id, channel_id FROM gateway_keys WHERE merchant_id = '$merchant_id' AND gateway = '$gateway'");
