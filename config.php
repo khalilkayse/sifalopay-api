a51879019c314411e064207d678da13bc6de9259
Khalil Ahmed <10650804+khalilkayse@users.noreply.github.com>
2024-05-15 09:05:26 -0700

    modified db connection

--- config.php
+++ config.php
@@ -2,38 +2,6 @@
 
 // db connection
 
-//old connection
-//$server = "spay.sifalo.com";
-//$user = "spay_admin";
-//$pass = "BUELFJet#28u";
-//$db =  "paydb"; 
-
-
-
-// if ($_SERVER['HTTP_HOST'] == "pay.sifalo.net" || $_SERVER['HTTP_HOST'] == "phpstack-889786-3206524.cloudwaysapps.com") {
-//   $server = "74.207.253.75";
-//   $user = "staging_admin";
-//   $pass = "1q2w3e4r-";
-//   $db =  "sifalo_pay_staging";
-// } elseif ($_SERVER['HTTP_HOST'] == "api.sifalopay.com" || $_SERVER['HTTP_HOST'] == "phpstack-889786-3084881.cloudwaysapps.com"){
-//   $server = "147.182.238.173";
-//   $user = "mybajnednq";
-//   $pass = "haRjJeP3Hp";
-//   $db =  "mybajnednq";
-// }
-
-// $con = mysqli_connect($server, $user, $pass, $db);
-
-
-
-
-// Database configuration file (db_config.php)
-// Use getenv() to retrieve environment variables
-
-
-// Secure connection script
-//require_once 'db_config.php'; // Include your database configuration file
-
 // Check the HTTP_HOST and assign the appropriate environment variables
 switch ($_SERVER['HTTP_HOST']) {
     case "pay.sifalo.net":
